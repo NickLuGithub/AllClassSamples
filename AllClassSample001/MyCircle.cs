f726cd024990255906360844e39e4b94754fbc8e
NickLu <word4205@gmail.com>
2023-02-22 14:22:21 +0800

    建新專案

---
+++ AllClassSample001/MyCircle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AllClassSample001
+{
+    internal partial class Program
+    {
+        public class MyCircle : MyShape
+        {
+            public double Redius { get; set; }
+            public override double GetArea()
+            {
+                return Math.PI * Math.Pow(Redius, 2);
+            }
+        }
+    }
+}
--- AllClassSample001/Program.cs
+++ AllClassSample001/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace AllClassSample001
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
+            MyShape rect = new MyRectangle() { Width = 10, Height = 10 };
+            Console.WriteLine($"方形的面積是 {rect.GetArea()}");
+            MyShape circle = new MyCircle() { Redius = 3 };
+            Console.WriteLine($"圓形的面積是 {circle.GetArea()}");
+
+            Console.ReadLine();
         }
     }
 }
