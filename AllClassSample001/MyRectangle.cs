10d15526c82c543289f33828cead477a9422c0ac
NickLu <word4205@gmail.com>
2023-02-22 13:55:22 +0800

    加入專案檔案。

---
+++ AllClassSample001/MyRectangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllClassSample001
+{
+    public class MyRectangle : MyShape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
---
+++ AllClassSample001/MyShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllClassSample001
+{
+    public abstract class MyShape
+    {
+        public abstract double GetArea();
+    }
+}
