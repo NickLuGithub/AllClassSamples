12f84e0231f9842420f884a41e3350e92d800b09
NickLu <word4205@gmail.com>
2023-02-22 14:27:43 +0800

    Class1

---
+++ AllClassSample002/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AllClassSample002
+{
+    internal partial class Program
+    {
+        public class Class1 : BaseClass
+        {
+            public override void Execute()
+            {
+                Console.WriteLine("Class1 Execute Method");
+            }
+        }
+    }
+}
